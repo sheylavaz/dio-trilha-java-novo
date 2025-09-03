89d138951cb5f7bd82429cea89746dc8332ad377
sheylavaz <sheylavaz@gmail.com>
2025-09-03 16:51:39 -0300

    Tratamento de exceção com try/catch, mensagem padronizada e constantes.

--- ContaBanco/src/br/ContaTerminal.java
+++ ContaBanco/src/br/ContaTerminal.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * @author sheyl
  */
 package br;
 
@@ -11,6 +11,9 @@ import java.util.Scanner;
  */
 public class ContaTerminal {
 
+	private static final String MENSAGEM_NUMERO_CONTA_CORRENTE_ERRO = "O numero da conta corrente deve ter somente numeros! Por favor revise!";
+	private static final String MENSAGEM_SALDO_ERRO = "Atenção: o valor informado no depósito está incorreto. Use ponto (.) em vez de vírgula (,) para os centavos, por favor revise!";	
+	
 	/**
 	 * 
 	 */
@@ -22,25 +25,36 @@ public class ContaTerminal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int numeroCc;
+		
+		Integer numeroCc = null;
 		String agencia;
 		String nomeCliente;
-		double saldo;
+		Double saldo = null;
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		try(sc){
+			
+			System.out.println("Por favor, digite o nome completo do cliente: ");
+			nomeCliente = sc.nextLine();
+			System.out.println("Digite o número da Agência: ");
+			agencia = sc.next();
+			System.out.println("Digita o número da conta corrente: ");
+			numeroCc = sc.nextInt();
+			System.out.println("Digita o valor de deposito: ");
+			saldo = sc.nextDouble();
+			System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque.\n", nomeCliente, agencia, numeroCc, saldo);
 		
-		System.out.println("Por favor, digite o nome completo do cliente: ");
-		nomeCliente = sc.nextLine();
-		System.out.println("Digite o número da Agência: ");
-		agencia = sc.next();
-		System.out.println("Digita o número da conta corrente: ");
-		numeroCc = sc.nextInt();
-		System.out.println("Digita o valor de deposito: ");
-		saldo = sc.nextDouble();
-		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque\n", nomeCliente, agencia, numeroCc, saldo);
-		
+		} catch (Exception e) {
+			if(numeroCc == null) {
+				System.out.println(MENSAGEM_NUMERO_CONTA_CORRENTE_ERRO);
+			}
+			if(saldo == null) {
+				System.out.println(MENSAGEM_SALDO_ERRO);
+			}
+		}
 		sc.close();
+		
 	}
 
 }
