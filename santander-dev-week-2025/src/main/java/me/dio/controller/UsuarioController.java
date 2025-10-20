683432dfca71a861ad34bc2452cd265bdaa28a92
sheylavaz <sheylavaz@gmail.com>
2025-10-20 12:05:32 -0300

    Adicionar novos metodos no controller - API RESTful

--- santander-dev-week-2025/src/main/java/me/dio/controller/UsuarioController.java
+++ santander-dev-week-2025/src/main/java/me/dio/controller/UsuarioController.java
@@ -12,7 +12,11 @@ package me.dio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.model.Usuario;
@@ -31,5 +35,19 @@ public class UsuarioController {
 	public List<Usuario> getUsers(){
 		return repository.findAll();
 	}
-
+	
+	@GetMapping("/users/{username}")
+	public Usuario getOne(@PathVariable("username") String username) {
+		return repository.findByUsername(username);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable("id") Integer id) {repository.deleteById(id);}
+	
+	@PostMapping("/users")
+	public void postUser(@RequestBody Usuario usuario) {
+		repository.save(usuario);
+	}
+	
+	
 }
